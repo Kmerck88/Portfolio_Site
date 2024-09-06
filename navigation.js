9d0765c10747b87367fb363f440320de0506da68
Kmerck88 <kevin.merck88@gmail.com>
2024-09-05 20:06:54 -0400

    Separated Home, About, and Contact into individual pages

---
+++ navigation.js
@@ -0,0 +1,10 @@
+// Function to simulate click on the link
+function navigateToSection(sectionId) {
+    document.getElementById(sectionId).click();
+}
+
+// Example usage
+navigateToSection('aboutLink'); // Navigate to "Meet ME" section
+navigateToSection('projectsLink'); // Navigate to "Projects" section
+navigateToSection('contactLink'); // Navigate to "Contact" section
+navigateToSection('resumeLink'); // Download Resume
